5862268600d8c87d7bd04f62bb22c2639b01666f
Arkkarachat  Siribout <arkkarachat.s@gmail.com>
2020-05-13 22:44:46 +0700

    change sql file

--- db/account.sql
+++ db/account.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2020 at 11:37 AM
+-- Generation Time: May 13, 2020 at 05:43 PM
 -- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.5
+-- PHP Version: 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,46 +24,115 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `account`
+--
+
+CREATE TABLE `account` (
+  `account_id` bigint(20) UNSIGNED NOT NULL,
+  `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `list` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `amount` double(8,2) NOT NULL,
+  `date` date NOT NULL,
+  `description` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `remove_status` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2020_05_13_142230_create_users_table', 1),
+(2, '2020_05_13_152120_create_account_table', 2);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
   `user_username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `user_password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `user_firstname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `user_lastname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `user_status` tinyint(4) NOT NULL DEFAULT 0,
-  `user_remove_status` tinyint(4) NOT NULL DEFAULT 0
+  `user_status` tinyint(4) NOT NULL,
+  `user_remove_status` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`user_id`, `user_username`, `user_password`, `user_firstname`, `user_lastname`, `user_status`, `user_remove_status`) VALUES
-(1, 'admin', '$2y$10$LE/5VfzWFmGD2SVFxTEWWOQlcfxb4kbDq7tmHXqWSSc4IjutkKkJq', 'Admin', 'Administrator', 9, 9),
-(2, 'arkkarachat', '$2y$10$LE/5VfzWFmGD2SVFxTEWWOQlcfxb4kbDq7tmHXqWSSc4IjutkKkJq', 'อัครชาติ', 'ศิริบุตร', 0, 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`account_id`),
+  ADD KEY `account_account_id_username_index` (`account_id`,`username`),
+  ADD KEY `account_username_foreign` (`username`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `users_user_username_unique` (`user_username`),
+  ADD KEY `users_user_id_user_username_index` (`user_id`,`user_username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `account`
+--
+ALTER TABLE `account`
+  MODIFY `account_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `account`
+--
+ALTER TABLE `account`
+  ADD CONSTRAINT `account_username_foreign` FOREIGN KEY (`username`) REFERENCES `users` (`user_username`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
